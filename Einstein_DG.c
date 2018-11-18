d83067f956dd8136009a9c7c589a3d7017e62a75
hslynn <hslsugar163.com>
2018-11-18 18:43:37 +0800

    backup

--- Einstein_DG.c
+++ Einstein_DG.c
@@ -35,12 +35,11 @@ main(int argc, char * argv[])
     //const char *matrix_fn = "mat.m", *var_name = "mat_M";
     GRID *g; 
     DOF_TYPE *dof_tp = DOF_DG2;
-    DOF *u_h, *p_0, *p_1, *q_0, *q_1, *w_0, *w_1;
-    //DOF *psi_00, *psi_01, *psi_02, *psi_03, *psi_11, *psi_12, *psi_13, *psi_22, *psi_23, *psi_33;
-    //DOF *Pi_00, *Pi_01, *Pi_02, *Pi_03, *Pi_11, *Pi_12, *Pi_13, *Pi_22, *Pi_23, *Pi_33;
-    //DOF *Phi_100, *Phi_101, *Phi_102, *Phi_103, *Phi_111, *Phi_112, *Phi_113, *Phi_122, *Phi_123, *Phi_133;
-    //DOF *Phi_200, *Phi_201, *Phi_202, *Phi_203, *Phi_211, *Phi_212, *Phi_213, *Phi_222, *Phi_223, *Phi_233;
-    //DOF *Phi_300, *Phi_301, *Phi_302, *Phi_303, *Phi_311, *Phi_312, *Phi_313, *Phi_322, *Phi_323, *Phi_333;
+    DOF *psi_00, *psi_01, *psi_02, *psi_03, *psi_11, *psi_12, *psi_13, *psi_22, *psi_23, *psi_33;
+    DOF *Pi_00, *Pi_01, *Pi_02, *Pi_03, *Pi_11, *Pi_12, *Pi_13, *Pi_22, *Pi_23, *Pi_33;
+    DOF *Phi_100, *Phi_101, *Phi_102, *Phi_103, *Phi_111, *Phi_112, *Phi_113, *Phi_122, *Phi_123, *Phi_133;
+    DOF *Phi_200, *Phi_201, *Phi_202, *Phi_203, *Phi_211, *Phi_212, *Phi_213, *Phi_222, *Phi_223, *Phi_233;
+    DOF *Phi_300, *Phi_301, *Phi_302, *Phi_303, *Phi_311, *Phi_312, *Phi_313, *Phi_322, *Phi_323, *Phi_333;
 
     //MAT *mat_M;
 
@@ -53,116 +52,91 @@ main(int argc, char * argv[])
     //phgRefineAllElements(g, 1);
    
     /*creat dofs for all the functions to be solved*/ 
-    //psi_00 = phgDofNew(g, dof_tp, 1, "psi_00", DofInterpolation);
-    //psi_01 = phgDofNew(g, dof_tp, 1, "psi_01", DofInterpolation);
-    //psi_02 = phgDofNew(g, dof_tp, 1, "psi_02", DofInterpolation);
-    //psi_03 = phgDofNew(g, dof_tp, 1, "psi_03", DofInterpolation);
-    //psi_11 = phgDofNew(g, dof_tp, 1, "psi_11", DofInterpolation);
-    //psi_12 = phgDofNew(g, dof_tp, 1, "psi_12", DofInterpolation);
-    //psi_13 = phgDofNew(g, dof_tp, 1, "psi_13", DofInterpolation);
-    //psi_22 = phgDofNew(g, dof_tp, 1, "psi_22", DofInterpolation);
-    //psi_23 = phgDofNew(g, dof_tp, 1, "psi_23", DofInterpolation);
-    //psi_33 = phgDofNew(g, dof_tp, 1, "psi_33", DofInterpolation);
+    psi_00 = phgDofNew(g, dof_tp, 1, "psi_00", DofInterpolation);
+    psi_01 = phgDofNew(g, dof_tp, 1, "psi_01", DofInterpolation);
+    psi_02 = phgDofNew(g, dof_tp, 1, "psi_02", DofInterpolation);
+    psi_03 = phgDofNew(g, dof_tp, 1, "psi_03", DofInterpolation);
+    psi_11 = phgDofNew(g, dof_tp, 1, "psi_11", DofInterpolation);
+    psi_12 = phgDofNew(g, dof_tp, 1, "psi_12", DofInterpolation);
+    psi_13 = phgDofNew(g, dof_tp, 1, "psi_13", DofInterpolation);
+    psi_22 = phgDofNew(g, dof_tp, 1, "psi_22", DofInterpolation);
+    psi_23 = phgDofNew(g, dof_tp, 1, "psi_23", DofInterpolation);
+    psi_33 = phgDofNew(g, dof_tp, 1, "psi_33", DofInterpolation);
 
-    //DOF *psi_components[4][4] = {{psi_00, psi_01, psi_02, psi_03}, 
-    //                  {psi_01, psi_11, psi_12, psi_13},
-    //                  {psi_02, psi_12, psi_22, psi_23},
-    //                  {psi_03, psi_13, psi_23, psi_33}};
+    DOF *dofs_psi[10] = {psi_00, psi_01, psi_02, psi_03, 
+                                 psi_11, psi_12, psi_13,
+                                         psi_22, psi_23,
+                                                 psi_33};
 
-    //Pi_00 = phgDofNew(g, dof_tp, 1, "Pi_00", DofInterpolation);
-    //Pi_01 = phgDofNew(g, dof_tp, 1, "Pi_01", DofInterpolation);
-    //Pi_02 = phgDofNew(g, dof_tp, 1, "Pi_02", DofInterpolation);
-    //Pi_03 = phgDofNew(g, dof_tp, 1, "Pi_03", DofInterpolation);
-    //Pi_11 = phgDofNew(g, dof_tp, 1, "Pi_11", DofInterpolation);
-    //Pi_12 = phgDofNew(g, dof_tp, 1, "Pi_12", DofInterpolation);
-    //Pi_13 = phgDofNew(g, dof_tp, 1, "Pi_13", DofInterpolation);
-    //Pi_22 = phgDofNew(g, dof_tp, 1, "Pi_22", DofInterpolation);
-    //Pi_23 = phgDofNew(g, dof_tp, 1, "Pi_23", DofInterpolation);
-    //Pi_33 = phgDofNew(g, dof_tp, 1, "Pi_33", DofInterpolation);
-    //
-    //DOF *Pi_components[4][4] = {{Pi_00, Pi_01, Pi_02, Pi_03}, 
-    //                 {Pi_01, Pi_11, Pi_12, Pi_13},
-    //                 {Pi_02, Pi_12, Pi_22, Pi_23},
-    //                 {Pi_03, Pi_13, Pi_23, Pi_33}};
+    Pi_00 = phgDofNew(g, dof_tp, 1, "Pi_00", DofInterpolation);
+    Pi_01 = phgDofNew(g, dof_tp, 1, "Pi_01", DofInterpolation);
+    Pi_02 = phgDofNew(g, dof_tp, 1, "Pi_02", DofInterpolation);
+    Pi_03 = phgDofNew(g, dof_tp, 1, "Pi_03", DofInterpolation);
+    Pi_11 = phgDofNew(g, dof_tp, 1, "Pi_11", DofInterpolation);
+    Pi_12 = phgDofNew(g, dof_tp, 1, "Pi_12", DofInterpolation);
+    Pi_13 = phgDofNew(g, dof_tp, 1, "Pi_13", DofInterpolation);
+    Pi_22 = phgDofNew(g, dof_tp, 1, "Pi_22", DofInterpolation);
+    Pi_23 = phgDofNew(g, dof_tp, 1, "Pi_23", DofInterpolation);
+    Pi_33 = phgDofNew(g, dof_tp, 1, "Pi_33", DofInterpolation);
+    
+    DOF *dofs_Pi[10] = {Pi_00, Pi_01, Pi_02, Pi_03, 
+                               Pi_11, Pi_12, Pi_13,
+                                      Pi_22, Pi_23,
+                                             Pi_33};
    
-    //Phi_100 = phgDofNew(g, dof_tp, 1, "Phi_100", DofInterpolation);
-    //Phi_101 = phgDofNew(g, dof_tp, 1, "Phi_101", DofInterpolation);
-    //Phi_102 = phgDofNew(g, dof_tp, 1, "Phi_102", DofInterpolation);
-    //Phi_103 = phgDofNew(g, dof_tp, 1, "Phi_103", DofInterpolation);
-    //Phi_111 = phgDofNew(g, dof_tp, 1, "Phi_111", DofInterpolation);
-    //Phi_112 = phgDofNew(g, dof_tp, 1, "Phi_112", DofInterpolation);
-    //Phi_113 = phgDofNew(g, dof_tp, 1, "Phi_113", DofInterpolation);
-    //Phi_122 = phgDofNew(g, dof_tp, 1, "Phi_122", DofInterpolation);
-    //Phi_123 = phgDofNew(g, dof_tp, 1, "Phi_123", DofInterpolation);
-    //Phi_133 = phgDofNew(g, dof_tp, 1, "Phi_133", DofInterpolation);
+    Phi_100 = phgDofNew(g, dof_tp, 1, "Phi_100", DofInterpolation);
+    Phi_101 = phgDofNew(g, dof_tp, 1, "Phi_101", DofInterpolation);
+    Phi_102 = phgDofNew(g, dof_tp, 1, "Phi_102", DofInterpolation);
+    Phi_103 = phgDofNew(g, dof_tp, 1, "Phi_103", DofInterpolation);
+    Phi_111 = phgDofNew(g, dof_tp, 1, "Phi_111", DofInterpolation);
+    Phi_112 = phgDofNew(g, dof_tp, 1, "Phi_112", DofInterpolation);
+    Phi_113 = phgDofNew(g, dof_tp, 1, "Phi_113", DofInterpolation);
+    Phi_122 = phgDofNew(g, dof_tp, 1, "Phi_122", DofInterpolation);
+    Phi_123 = phgDofNew(g, dof_tp, 1, "Phi_123", DofInterpolation);
+    Phi_133 = phgDofNew(g, dof_tp, 1, "Phi_133", DofInterpolation);
 
-    //Phi_200 = phgDofNew(g, dof_tp, 1, "Phi_200", DofInterpolation);
-    //Phi_201 = phgDofNew(g, dof_tp, 1, "Phi_201", DofInterpolation);
-    //Phi_202 = phgDofNew(g, dof_tp, 1, "Phi_202", DofInterpolation);
-    //Phi_203 = phgDofNew(g, dof_tp, 1, "Phi_203", DofInterpolation);
-    //Phi_211 = phgDofNew(g, dof_tp, 1, "Phi_211", DofInterpolation);
-    //Phi_212 = phgDofNew(g, dof_tp, 1, "Phi_212", DofInterpolation);
-    //Phi_213 = phgDofNew(g, dof_tp, 1, "Phi_213", DofInterpolation);
-    //Phi_222 = phgDofNew(g, dof_tp, 1, "Phi_222", DofInterpolation);
-    //Phi_223 = phgDofNew(g, dof_tp, 1, "Phi_223", DofInterpolation);
-    //Phi_233 = phgDofNew(g, dof_tp, 1, "Phi_233", DofInterpolation);
+    Phi_200 = phgDofNew(g, dof_tp, 1, "Phi_200", DofInterpolation);
+    Phi_201 = phgDofNew(g, dof_tp, 1, "Phi_201", DofInterpolation);
+    Phi_202 = phgDofNew(g, dof_tp, 1, "Phi_202", DofInterpolation);
+    Phi_203 = phgDofNew(g, dof_tp, 1, "Phi_203", DofInterpolation);
+    Phi_211 = phgDofNew(g, dof_tp, 1, "Phi_211", DofInterpolation);
+    Phi_212 = phgDofNew(g, dof_tp, 1, "Phi_212", DofInterpolation);
+    Phi_213 = phgDofNew(g, dof_tp, 1, "Phi_213", DofInterpolation);
+    Phi_222 = phgDofNew(g, dof_tp, 1, "Phi_222", DofInterpolation);
+    Phi_223 = phgDofNew(g, dof_tp, 1, "Phi_223", DofInterpolation);
+    Phi_233 = phgDofNew(g, dof_tp, 1, "Phi_233", DofInterpolation);
 
-    //Phi_300 = phgDofNew(g, dof_tp, 1, "Phi_300", DofInterpolation);
-    //Phi_301 = phgDofNew(g, dof_tp, 1, "Phi_301", DofInterpolation);
-    //Phi_302 = phgDofNew(g, dof_tp, 1, "Phi_302", DofInterpolation);
-    //Phi_303 = phgDofNew(g, dof_tp, 1, "Phi_303", DofInterpolation);
-    //Phi_311 = phgDofNew(g, dof_tp, 1, "Phi_311", DofInterpolation);
-    //Phi_312 = phgDofNew(g, dof_tp, 1, "Phi_312", DofInterpolation);
-    //Phi_313 = phgDofNew(g, dof_tp, 1, "Phi_313", DofInterpolation);
-    //Phi_322 = phgDofNew(g, dof_tp, 1, "Phi_322", DofInterpolation);
-    //Phi_323 = phgDofNew(g, dof_tp, 1, "Phi_323", DofInterpolation);
-    //Phi_333 = phgDofNew(g, dof_tp, 1, "Phi_333", DofInterpolation);
+    Phi_300 = phgDofNew(g, dof_tp, 1, "Phi_300", DofInterpolation);
+    Phi_301 = phgDofNew(g, dof_tp, 1, "Phi_301", DofInterpolation);
+    Phi_302 = phgDofNew(g, dof_tp, 1, "Phi_302", DofInterpolation);
+    Phi_303 = phgDofNew(g, dof_tp, 1, "Phi_303", DofInterpolation);
+    Phi_311 = phgDofNew(g, dof_tp, 1, "Phi_311", DofInterpolation);
+    Phi_312 = phgDofNew(g, dof_tp, 1, "Phi_312", DofInterpolation);
+    Phi_313 = phgDofNew(g, dof_tp, 1, "Phi_313", DofInterpolation);
+    Phi_322 = phgDofNew(g, dof_tp, 1, "Phi_322", DofInterpolation);
+    Phi_323 = phgDofNew(g, dof_tp, 1, "Phi_323", DofInterpolation);
+    Phi_333 = phgDofNew(g, dof_tp, 1, "Phi_333", DofInterpolation);
 
-    //DOF *Phi[3][4][4] ={{{Phi_100, Phi_101, Phi_102, Phi_103}, 
-    //                     {Phi_101, Phi_111, Phi_112, Phi_113},
-    //                     {Phi_102, Phi_112, Phi_122, Phi_123},
-    //                     {Phi_103, Phi_113, Phi_123, Phi_133}}, 
-    //                    {{Phi_200, Phi_201, Phi_202, Phi_203},
-    //                     {Phi_201, Phi_211, Phi_212, Phi_213},
-    //                     {Phi_202, Phi_212, Phi_222, Phi_223},
-    //                     {Phi_203, Phi_213, Phi_223, Phi_233}},
-    //                    {{Phi_300, Phi_301, Phi_302, Phi_303},
-    //                     {Phi_301, Phi_311, Phi_312, Phi_313},
-    //                     {Phi_302, Phi_312, Phi_322, Phi_323},
-    //                     {Phi_303, Phi_313, Phi_323, Phi_333}}};
-
-    u_h = phgDofNew(g, dof_tp, 1, "u_h", func_u);
-    p_0 = phgDofNew(g, dof_tp, 1, "p_0", DofInterpolation); 
-    p_1 = phgDofNew(g, dof_tp, 1, "p_1", DofInterpolation); 
-    q_0 = phgDofNew(g, dof_tp, 1, "q_0", DofInterpolation); 
-    q_1 = phgDofNew(g, dof_tp, 1, "q_1", DofInterpolation); 
-    w_0 = phgDofNew(g, dof_tp, 1, "w_0", DofInterpolation); 
-    w_1 = phgDofNew(g, dof_tp, 1, "w_1", DofInterpolation); 
-    
-    dgHJGradDof(u_h, p_0, p_1, q_0, q_1, w_0, w_1);     
+    DOF *dofs_Phi[30] ={Phi_100, Phi_101, Phi_102, Phi_103, 
+                   Phi_111, Phi_112, Phi_113,
+                   Phi_122, Phi_123,
+                   Phi_133, 
+                   Phi_200, Phi_201, Phi_202, Phi_203,
+                   Phi_211, Phi_212, Phi_213,
+                   Phi_222, Phi_223,
+                   Phi_233,
+                   Phi_300, Phi_301, Phi_302, Phi_303,
+                   Phi_311, Phi_312, Phi_313,
+                   Phi_322, Phi_323,
+                   Phi_333};
 
     
-    printf("test\n");
+    //printf("test\n");
     /*Export VTK*/
-    phgExportVTK(g, "tmp.vtk", u_h, p_0, p_1, q_0, q_1, w_0, w_1, NULL);
+    //phgExportVTK(g, "tmp.vtk", u_h, p_0, p_1, q_0, q_1, w_0, w_1, NULL);
 
     /*release the mem*/
-    //for(i=0;i<4;i++)
-    //    for(j=0;j<=i;j++){
-    //        phgDofFree(&psi_components[i][j]);
-    //        phgDofFree(&Pi_components[i][j]);
-    //        for(k=0;k<3;k++){
-    //            phgDofFree(&Phi_components[k][i][j]);
-    //        }
-    //    }
 
-    phgDofFree(&u_h); 
-    phgDofFree(&p_0); 
-    phgDofFree(&p_1);
-    phgDofFree(&q_0);
-    phgDofFree(&q_1);
-    phgDofFree(&w_0);
-    phgDofFree(&w_1);
 
     phgFreeGrid(&g); 
  
---
+++ Hhat.c
@@ -0,0 +1,6 @@
+#define alpha 1.0
+#define beta 1.0
+#define sigma 1.0
+
+static void
+
